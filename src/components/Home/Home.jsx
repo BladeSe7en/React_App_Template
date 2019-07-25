f2f42c7843b52226397be318f11ab4c661ff96b9
BrianC <seahorse8789@gmail.com>
2019-07-25 14:27:33 -0700

    This is a base template for creating react apps

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>Hello World </h1>
             </div>
         )
     }
